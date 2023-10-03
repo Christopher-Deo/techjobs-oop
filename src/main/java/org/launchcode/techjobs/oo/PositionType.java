8360b0f82580006e635d0786b893f8e5534fccae
Christopher-Deo <chris@christopherdeo.com>
2023-10-03 08:35:14 -0500

    Created abstract base class

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,54 +2,14 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType {
-
-    private int id;
-    private static int nextId = 1;
-    private String value;
+public class PositionType extends JobField{
 
     public PositionType() {
-        id = nextId;
-        nextId++;
+        super(); // Calls the constructor of the superclass (JobField)
     }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    // Custom toString() method that returns the data stored in 'value'.
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    // Custom equals() method to compare PositionType objects based on their id fields.
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return id == that.id;
-    }
-
-    // Custom hashCode() method that generates a hash code based on the id field.
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        super(value); // Calls the constructor of the superclass (JobField) with the provided value
+        // You can initialize any additional fields specific to the CoreCompetency class here.
     }
 }
